49b884270e9b3840ee6b403caaa7062eb73cc4cf
mariasolegallina <gallinamariasole@gmail.com>
2023-10-03 14:39:22 +0200

    add - prompt userName

---
+++ js/app.js
@@ -0,0 +1,24 @@
+// - chidere all'utente il suo nome:
+//   - dichiarare una variabile
+let userName
+//   - assegnare alla variabile il valore restituito da un prompt
+userName = prompt ("Qual è il tuo nome?")
+console.log(userName)
+
+// - chidere all'utente il suo cognome:
+//   - dichiarare una variabile
+//   - assegnare alla variabile il valore restituito da un prompt
+
+// - chidere all'utente il suo colore preferito:
+//   - dichiarare una variabile
+//   - assegnare alla variabile il valore restituito da un prompt
+
+// - coincatenare il nome, il cognome e il colore preferito dell'utente con un valore numerico:
+//   - dichiarare una variabile che contiene il valore numerico
+//   - dichiarare una variabile message dove concatenare le quattro stringhe
+//   - dichiarare una variabile con le quattro stringhe concatenate
+
+// - stampare il messaggio dentro un p:
+//   - inserire nell'html un tag p con id="passwordCombination"
+//   - recuperare con js l'elementi del dom con id = "passwordCombination"
+//   - modificare l'innerHTML del p con il valore della variabile "passwordCombination"
