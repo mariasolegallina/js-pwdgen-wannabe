74da75c7b6929a1d62b527e292fd02395a26e432
mariasolegallina <gallinamariasole@gmail.com>
2023-10-03 14:43:57 +0200

    add - userLastname, userFavouriteColor

--- js/app.js
+++ js/app.js
@@ -1,17 +1,23 @@
 // - chidere all'utente il suo nome:
 //   - dichiarare una variabile
-let userName
+let userName;
 //   - assegnare alla variabile il valore restituito da un prompt
-userName = prompt ("Qual è il tuo nome?")
-console.log(userName)
+userName = prompt ("Qual è il tuo nome?");
+console.log(userName);
 
 // - chidere all'utente il suo cognome:
 //   - dichiarare una variabile
+let userLastname;
 //   - assegnare alla variabile il valore restituito da un prompt
+userLastname = prompt ("Qual è il tuo cognome?");
+console.log(userLastname);
 
 // - chidere all'utente il suo colore preferito:
 //   - dichiarare una variabile
+let userFavouriteColour;
 //   - assegnare alla variabile il valore restituito da un prompt
+userFavouriteColour = prompt ("Qual è il tuo colore preferito?");
+console.log(userFavouriteColour);
 
 // - coincatenare il nome, il cognome e il colore preferito dell'utente con un valore numerico:
 //   - dichiarare una variabile che contiene il valore numerico
